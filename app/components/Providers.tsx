49dad975deb4b8e9581b6f22f6c5291846094fcc
junseokku <asegh12@gmail.com>
2025-05-06 15:08:44 +0900

    fix: 클라이언트에서 테마 프로바이더 마운트하게 하는 것 제거 - 하이드레이션 미스매치 에러로 추가했지만, 테마 변경으로 인한 깜빡임

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -1,17 +1,9 @@
 'use client';
 
-import { useMounted } from '@/utils/useMounted';
 import { ThemeProvider } from 'next-themes';
-import { Fragment, PropsWithChildren } from 'react';
+import { PropsWithChildren } from 'react';
 
 export const Providers = ({ children }: PropsWithChildren) => {
-  return <ClientOnlyThemeProvider>{children}</ClientOnlyThemeProvider>;
-};
-
-const ClientOnlyThemeProvider = ({ children }: PropsWithChildren) => {
-  const mounted = useMounted();
-  if (!mounted) return <Fragment>{children}</Fragment>;
-
   return (
     <ThemeProvider
       attribute="class"
